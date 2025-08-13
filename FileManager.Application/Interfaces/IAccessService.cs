316a966bbb0487404853e5c12ff814f58254f522
Ilay3 <93222397+Ilay3@users.noreply.github.com>
2025-08-13 10:34:20 +0400

    Добавлен флаг allowOwner и запись прав владельца

--- FileManager.Application/Interfaces/IAccessService.cs
+++ FileManager.Application/Interfaces/IAccessService.cs
@@ -8,7 +8,7 @@ public interface IAccessService
     Task<List<AccessRuleDto>> GetFileAccessAsync(Guid fileId);
     Task<List<AccessRuleDto>> GetFolderAccessAsync(Guid folderId);
     Task GrantAccessAsync(Guid? fileId, Guid? folderId, Guid? userId, Guid? groupId,
-        AccessType accessType, Guid grantedById, bool inherit = true);
+        AccessType accessType, Guid grantedById, bool inherit = true, bool allowOwner = false);
     Task<bool> RevokeAccessAsync(Guid accessRuleId, Guid revokedById);
     Task<AccessType> GetEffectiveAccessAsync(Guid userId, Guid fileId);
     Task<AccessType> GetEffectiveFolderAccessAsync(Guid userId, Guid folderId);
